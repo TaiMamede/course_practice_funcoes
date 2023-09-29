5107d79f1fba368284967641850c66b3ddfcff3f
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-29 16:00:17 +0100

    refactor code

--- funcoes4.py
+++ funcoes4.py
@@ -1,16 +1,17 @@
-"""
-Funções com Parâmetro Patrao
-Função chamada potencia: com um parâmetro calcula o quadrado do numero, dois parâmetros o segundo
-será utilizado como o valor da potencial.
-"""
-
 def potencia(numero, expoente=2):
+    """Função que calcula a potencia de um número valor de Entrada:
+    numero - numero a ser calculado (elevado a potencia(float))
+    expoente - expoente a ser utlizado no calculo(inteiro)
+
+    Resultado:
+    Resultado ao calculo da potencia(float)"""
     resultado = pow(numero, expoente)
     return resultado
 
 n = float(input("Digite o número: "))
 e = int(input("Expoente: "))
 
+
 print(f"Valor com expoente: {potencia(expoente=e, numero=n)}")
 print(f"Valor sem expoente: {potencia(n)}")
-
+help(potencia)
